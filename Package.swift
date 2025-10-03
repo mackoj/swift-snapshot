96d64b43144af880be76dd6985e722005dcfa730
Jeffrey Macko <jeffrey.macko@decathlon.com>
2025-10-03 23:58:11 +0200

    fix conccurrency

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 6.1
 import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
   name: "swift-snapshot",
-  platforms: [.macOS(.v13), .iOS(.v15)],
+  platforms: [.macOS(.v13), .iOS(.v16), .watchOS(.v9), .tvOS(.v16)],
   products: [
     .library(
       name: "SwiftSnapshot",
@@ -16,10 +14,10 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"603.0.0"),
     .package(url: "https://github.com/swiftlang/swift-format", "509.0.0"..<"603.0.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.0"),
-    .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.5.0"),
-    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.2.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.7.0"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.18.0"),
+    .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.6.0"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.10.0"),
   ],
   targets: [
     // Macro implementation (compiler plugin + public macro definitions)
@@ -34,7 +32,7 @@ let package = Package(
       ],
       path: "Sources/SwiftSnapshotMacros"
     ),
-
+    
     // Core runtime library implementation
     .target(
       name: "SwiftSnapshotCore",
@@ -48,7 +46,7 @@ let package = Package(
       ],
       path: "Sources/SwiftSnapshotCore"
     ),
-
+    
     // Unified import module (re-exports Core + Macros)
     .target(
       name: "SwiftSnapshot",
@@ -58,7 +56,7 @@ let package = Package(
       ],
       path: "Sources/SwiftSnapshot"
     ),
-
+    
     // Core runtime tests
     .testTarget(
       name: "SwiftSnapshotTests",
@@ -68,7 +66,7 @@ let package = Package(
         .product(name: "InlineSnapshotTesting", package: "swift-snapshot-testing"),
       ]
     ),
-
+    
     // Macro tests
     .testTarget(
       name: "SwiftSnapshotMacrosTests",
@@ -79,5 +77,27 @@ let package = Package(
         .product(name: "MacroTesting", package: "swift-macro-testing"),
       ]
     ),
-  ]
+  ],
+  swiftLanguageModes: [.v6]
 )
+
+let swiftSettings: [SwiftSetting] = [
+  .enableUpcomingFeature("MemberImportVisibility")
+  // .unsafeFlags([
+  //   "-Xfrontend",
+  //   "-warn-long-function-bodies=50",
+  //   "-Xfrontend",
+  //   "-warn-long-expression-type-checking=50",
+  // ])
+]
+
+for index in package.targets.indices {
+  package.targets[index].swiftSettings = swiftSettings
+}
+
+#if !os(Windows)
+// Add the documentation compiler plugin if possible
+package.dependencies.append(
+  .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.0")
+)
+#endif
