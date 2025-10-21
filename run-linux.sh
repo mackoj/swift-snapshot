ef3524cf4e0f0923d0e5d7689e73760ca039b9d8
Jeffrey Macko <mackoj@users.noreply.github.com>
2025-10-21 16:51:34 +0200

    Update run-linux.sh

--- run-linux.sh
+++ run-linux.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PROJECT_DIR="/Users/mac-JMACKO01/Developer/swift-snapshot-mackoj"
-
 # Ensure container system is running
 container system start || true
 
 # Run tests
 container run \
-  --volume "${PROJECT_DIR}:/workspace" \
+  --volume ".:/workspace" \
   --workdir "/workspace" \
   --tty \
   --interactive \
